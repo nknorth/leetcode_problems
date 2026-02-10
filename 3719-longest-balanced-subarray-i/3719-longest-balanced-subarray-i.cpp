fb673f3aac1423988580445ab02876761592d9f5
NANDHAKUMAR S <123979698+nknorth@users.noreply.github.com>
2026-02-10 21:16:52 +0530

    Time: 2136 ms (14.05%), Space: 473.3 MB (59.54%) - LeetHub

---
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n=nums.size();
+       int res=0;
+    for(int i=0;i<n;i++){
+            set<int>e,o;
+            for(int j=i;j<n;j++){
+            if(nums[j]%2==0){
+                e.insert(nums[j]);
+            }else {
+                o.insert(nums[j]);
+            }if(e.size()==o.size()){
+                res=max(res,j-i+1);
+            }                
+            }
+        }
+        return res;
+    }
+};
